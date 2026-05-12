9c81650dfdf958efddbe01716bfc0aa1e59a79da
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-05-12 18:38:37 +0700

    menambah class ibu

---
+++ ibu.h
@@ -0,0 +1,26 @@
+#ifndef IBU_H
+#define IBU_H
+
+#include <iostream>
+#include <string>
+#include "anak.h"
+using namespace std;
+
+#include <vector>
+
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+    ibu(string pNama) : nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
+
