8d05c91924a1e354464e050f0d6790e40904b70a
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-05-12 18:15:55 +0700

    menambah prosedur tambah pasien dan cetak pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -25,8 +25,9 @@ public:
     dokter(string pNama) : nama(pNama) {
         cout << "Dokter \"" << nama << "\" ada\n";
     }
-
     ~dokter() {
         cout << "Dokter \"" << nama << "\" tidak ada\n";
     }
+    void tambahPasien(pasien*);
+    void cetakPasien();
 };
