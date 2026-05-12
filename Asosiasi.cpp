291e77ddfe1aef012da8f5cb6fcbc02d19912efc
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-05-12 18:19:12 +0700

    Menambahkan implementasi tambahDokter dan cetakDokter pada class pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -31,3 +31,12 @@ public:
     void tambahPasien(pasien*);
     void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+
+void pasien::cetakDokter() {
+    cout << "Daftar Dokter yang menangani pasien \"" 
+         << this->nama << "\":\n";
+}
